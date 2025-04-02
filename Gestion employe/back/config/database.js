a1c0398adc2943a66b68fe2389f021a4b1c5f5ba
FanouRFC <fanouomega@gmail.com>
2025-04-02 19:52:27 +0300

    mot de passe a changer

--- Gestion employe/back/config/database.js
+++ Gestion employe/back/config/database.js
@@ -2,7 +2,7 @@ const {Sequelize} = require('sequelize')
 
 // SOLOY MOT DE PASSE ANLE POSTGRESQL ANLISANY NY PARAMETRE FAHATELO @ Constructeur Sequelize io
 
-const sequelize = new Sequelize('etablissement', 'postgres', 'fafa', {
+const sequelize = new Sequelize('etablissement', 'postgres', '_MOT_DE_PASSE', {
     host: 'localhost',
     dialect: 'postgres'
 })
